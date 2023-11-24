1bb49c24940978c28d0e8ca4e681bdced4a2effc
Kin <winterheat@gmail.com>
2023-11-23 23:13:48 -0800

    good

---
+++ cypress/e2e/spec2.cy.js
@@ -0,0 +1,13 @@
+describe("template spec", () => {
+  it("passes", () => {
+    cy.visit("https://google.com");
+
+    cy.get("body").should("contain", "Lucky");
+  });
+
+  it("passes 2", () => {
+    cy.visit("https://google.com");
+
+    cy.get("body").should("contain", "Lucky2");
+  });
+});
